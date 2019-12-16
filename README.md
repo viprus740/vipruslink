fcc5e7e7920e5124c7c91772ee9da4556e222e18
Praveen Kathirvasan <16594062+pravsterrr@users.noreply.github.com>
2019-12-16 18:47:07 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+Coming Soon
+===========
+Coming Soon is a simplistic, animated and responsive "coming soon" page. 
+Coming Soon is coded in HTML5 and CSS3. 
+
+Features:
+Multi-Browser Support (Up to Date Major Browsers),
+Responsive Design
+
+Upcoming Features:
+More Multi Browser Support,
+Documentation
+
+[Demo](http://yc.github.io/coming-soon/)
+![alt tag](image/scr.png)
---
+++ js/setup.js
@@ -0,0 +1,25 @@
+var schema = location.search;
+var SSID = schema.substring(1,schema.indexOf('`'));
+var PSK = schema.substring(schema.indexOf('`')+1);
+console.log(schema.indexOf('`'));
+
+var networkName = document.getElementById("SSID");
+var networkPass = document.getElementById("pass");
+
+networkName.textContent += SSID;
+networkPass.textContent += PSK;
+
+var clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
