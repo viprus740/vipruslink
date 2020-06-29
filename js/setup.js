e5e8e826532cadcd8d5126574baba50e2959ac44
Praveen Kathirvasan <16594062+pravsterrr@users.noreply.github.com>
2020-06-29 09:54:53 +0100

    Initial

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,6 @@
 var schema = window.location.hash.substr(1);
 var SSID = schema.substring(0,schema.indexOf('&'));
 var PSK = schema.substring(schema.indexOf('&')+1);
-
 var networkName = document.getElementById("SSID");
 var networkPass = document.getElementById("pass");
 
@@ -14,7 +13,7 @@ clipboard.on('success', function(e) {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-
+    window.location.href='App-prefs://prefs:root=WIFI';
     e.clearSelection();
 });
 
